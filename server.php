4b591d5927a757f30b2f1590446e085f893b8ce9
nandoferth <vizcainolopezfernando1996@outlook.es>
2023-06-18 15:56:57 -0600

    refactor: The structure on the server was changed to receive an associative array.

--- server.php
+++ server.php
@@ -1,12 +1,12 @@
 <?php
 
-$conceptos = json_decode(file_get_contents('php://input'));
+$conceptos = json_decode(file_get_contents('php://input'), true);
 $renglon_5_6 = '';
-for($i = 0; $i<count($conceptos); $i++){
-    if($i<2){ continue; } // Ignorar las primeras 2 filas
-    $renglon_concepto = "\ncto;{$conceptos[$i][7]};servicio;E48;80101500;;{$conceptos[$i][1]} {$conceptos[$i][2]} {$conceptos[$i][3]};{$conceptos[$i][4]};{$conceptos[$i][4]};;02";
-    $importe = number_format($conceptos[$i][4]*0.160000, 2, '.', '');
-    $renglon_concepto_impuesto_retenido = "\nctoit;{$conceptos[$i][4]};002;Tasa;0.160000;{$importe}";
+foreach($conceptos as $concepto){
+    $datos_concepto = implode(';', array_values($concepto));
+    $renglon_concepto = "\ncto;{$datos_concepto}";
+    $importe = number_format($concepto['importe']*0.160000, 2, '.', '');
+    $renglon_concepto_impuesto_retenido = "\nctoit;{$concepto['importe']};002;Tasa;0.160000;{$importe}";
     $renglon_5_6 = $renglon_5_6.$renglon_concepto.$renglon_concepto_impuesto_retenido;
     // Cantidad;Unidad;ClaveUnidad;ClaveProdServ;NoIdentificacion;Descripcion;ValorUnitario;Importe;Descuento;ObjetoImp
 }
